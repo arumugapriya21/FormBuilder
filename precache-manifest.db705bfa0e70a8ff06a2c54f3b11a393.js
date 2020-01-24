dc08c981973c05c26e8fc4f25f0d657a2f210b5d
arumuga priya <arumugapriyai@teknuance.com>
2020-01-24 17:11:42 +0530

    Updates

---
+++ precache-manifest.db705bfa0e70a8ff06a2c54f3b11a393.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10cfe6332c947d4dced99b2317a0057",
+    "url": "/FormBuilder/index.html"
+  },
+  {
+    "revision": "4925c7ef03be234d432a",
+    "url": "/FormBuilder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "91fae2e116b7c852f878",
+    "url": "/FormBuilder/static/js/2.f99c009b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FormBuilder/static/js/2.f99c009b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4925c7ef03be234d432a",
+    "url": "/FormBuilder/static/js/main.8a690bb3.chunk.js"
+  },
+  {
+    "revision": "3ffb6895f749eec5be60",
+    "url": "/FormBuilder/static/js/runtime-main.bb81a6b2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/FormBuilder/static/media/logo.5d5d9eef.svg"
+  }
+]);
